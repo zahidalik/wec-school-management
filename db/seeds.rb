14550ca9a111e189f5b80919e9e803dbe6f2150a
zahidalik <zahidkhan545@icloud.com>
2022-06-28 21:26:34 +0300

    a change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Role.create!([{name: "super_admin"}, {name: "accountant"}, {name: "cashier"}, {name: "ceo"}, {name: "cfc"}, {name: "director"},
+Role.create!([{name: "accountant"}, {name: "cashier"}, {name: "ceo"}, {name: "cfc"}, {name: "director"},
             {name: "chairman"}, {name: "kitchen_supervisor"}, {name: "uans_headmaster"},
             {name: "kemps_headmaster"}, {name: "wbs_headmaster"}, {name: "wgs_headmaster"},
             {name: "cttc_principal"}, {name: "journalism_principal"}, {name: "nursing_principal"},
